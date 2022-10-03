dc9db0440cf311361d110c29428da047f9c2915d
Fernando Panduro <fernandogp2003pro@gmail.com>
2022-10-03 11:32:11 -0600

    Agregado contador interactivo con sus estilos de css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import './App.css';
 import './css/Normalize.css';
 import Button from './components/Button';
+import Counter from './components/Counter';
+import { useState } from 'react';
 
 function App() {
+
+  const [numClick, setNumClicks] = useState(0);
+
+  const manejarClick = () => {
+    setNumClicks(numClick + 1);
+  };
+
+
+  const reiniciarContador = () => {
+    setNumClicks(0);
+  };
 
   return (
     <div className="App">
 
-      <h1>Contador De Clicks</h1>
+      <div className='contenedor-margen contenedor-principal'>
 
-      <div className='btns'>
-        <Button
-        text='Click'
-        btn={true} />
-        <Button
-        text='Restart'
-        btn={true} />
+        <h1 className='title'>Contador De Clicks</h1>
+
+        <Counter 
+        numCounter={numClick} />
+
+        <div className='btns'>
+          <Button
+          text='Click'
+          btn={true}
+          onClick={manejarClick} />
+          <Button
+          text='Restart'
+          btn={true}
+          onClick={reiniciarContador} />
+        </div>
+
       </div>
         
     </div>
