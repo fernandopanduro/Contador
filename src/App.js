2be14c4a8cfdece7b07d13d7224984fae9789589
Fernando Panduro <fernandogp2003pro@gmail.com>
2022-10-03 10:49:13 -0600

    Estilo del componente voton terminado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import './css/Normalize.css';
+import Button from './components/Button';
 
 function App() {
+
   return (
     <div className="App">
- 
+
+      <h1>Contador De Clicks</h1>
+
+      <div className='btns'>
+        <Button
+        text='Click'
+        btn={true} />
+        <Button
+        text='Restart'
+        btn={true} />
+      </div>
+        
     </div>
   );
+
 }
 
 export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,2 +1,12 @@
 import React from 'react';
 import '../css/Button.css';
+
+function Button({ text, btn, onClick }) {
+    return (
+        <button className={btn ? 'btnClick' : 'btnRestart'} onClick={onClick}>
+            {text}
+        </button>
+    );
+}
+
+export default Button;
